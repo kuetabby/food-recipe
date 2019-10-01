7051d77e871ccfffd581a5e698931260143a55e9
Supreeth Baliga <supreethbaliga@gmail.com>
2019-10-01 12:51:47 +0530

    Added 7 more recipes to the recipes file.

--- src/containers/Recipe.tsx
+++ src/containers/Recipe.tsx
@@ -30,6 +30,49 @@ const Recipe = [
       "1 tbsp vegetable oil\n\n3 garlic cloves, chopped\n\n3 x 400g cans black beans, drained and rinsed\n\n3 tbsp cider vinegar\n\n1 ½ tbsp honey\n\n1 ½ tbsp smoked paprika\n\n1 ½ tbsp ground cumin\n\n1 small garlic clove\n\n2 tbsp roughly chopped coriander\n\n1 green chilli, sliced\n\n2 avocados, halved and stoned\n\njuice 1 lime\n\n110g pack pomegranate seeds\n\n1 green chilli, finely diced\n\n1 small white onion, finely diced\n\nsmall handful fresh coriander, chopped",
     img:
       "http://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/05/spicy-black-bean-tacos.jpg?itok=RTBXRL7L"
+  },
+  {
+    id:4,
+    title: "Creamy Potato Salad With Bacon",
+    ingredients:
+    "3 pounds small red new potatoes (about 24),kosher salt and black pepper, 8 slices bacon, 1/2 cup mayonnaise, 1/4 cup sour cream, 3 tablespoons white wine vinegar, 4 stalks celery, thinly sliced 1/2 cup chopped fresh flat-leaf parsley, 1/4 cup chopped fresh tarragon ",
+    img: "https://www.keyingredient.com/media/91/dc/5be0d6a05b33b89e6e04794a0f73460e3acf.jpg/rh/bacon-ranch-sour-cream-potato-salad.jpg"
+  },
+  {
+    id: 5,
+    title: "Chile Corn Fritters",
+    ingredients: "2 quarts canola oil ,1¼ cups medium-grind yellow cornmeal ,1¼ cups (about 5⅜ oz.) all-purpose flour ,2 tablespoons granulated sugar, 1½ teaspoons baking powder ,2 teaspoons kosher salt, divided 2 large eggs, lightly beaten 1 cup whole buttermilk ,2 tablespoons unsalted butter, melted 2 cups fresh yellow corn kernels (4 ears), 2 (4½-oz.) cans hot diced green chiles, drained ¼ cup sliced scallions (from 1 bunch) ",
+    img: "https://cdn-ap-ec.yottaa.net/57eaa34c2bb0ac1299000018/www.foodinaminute.co.nz/v~4b.2f/var/fiam/storage/images/recipes/chilli-corn-fritters/1285033-13-eng-US/Chilli-Corn-Fritters_recipeimage.jpg?yocs=5_&yoloc=ap"
+  },
+  {
+    id: 6,
+    title: "Buffalo Chicken",
+    ingredients: "1½ kg chicken thighs and drumsticks, 180g sea salt flakes, 90g soft dark brown sugar, 2 tbsp chilli flakes,     2 tbsp sweet smoked paprika, 1 tbsp ground cumin, 1 tbsp sea salt flakes, 1 tbsp dark brown sugar",
+    img: "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/11/buffalo-wings.jpg?itok=_N9D87_0"
+  },
+  {
+    id: 7,
+    title: "Texas BBQ medley",
+    ingredients: "1 large chicken, 5 beef short ribs (about 3kg), ribs separated 2kg piece boned pork shoulder, rind removed, Texas barbecue sauce, sliced sweet white onions, Onion and sliced gherkins, to serve",
+    img: "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/07/texas-bbq-medley-.jpg?itok=5oSXHSK4"
+  },
+  {
+    id: 8,
+    title: "S'mores dip",
+    ingredients: "200g milk chocolate, Milk chocolate squares, 2 tbsp full-fat milk, 350g mini marshmallows, 100g digestive biscuits, 100g strawberries, washed and hulled, 100g pineapple, Pineapple, chunks, skewers",
+    img: "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2016/09/smores-dip.jpg?itok=uGd_7cor"
+  },
+  {
+    id: 9,
+    title: "Fully loaded Cajun chicken burgers",
+    ingredients: "4 skinless chicken breasts,2 tbsp olive oil,4 rashers smoked bacon, 2 avocados, 4 ciabatta rolls, 4 thin slices of your favourite cheese, 4 small handfuls baby spinach leaves, mayonnaise, to serve (optional)",
+    img: "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--17429_11.jpg?itok=7lW0nrDJ"
+  },
+  {
+    id: 10,
+    title: "Cheesesteak hot dogs",
+    ingredients: "1 large rib-eye steak, 3 tbsp olive oil, 2 garlic cloves, crushed but whole, 2 green peppers, sliced, 4 hot dog buns, 60g smoked cheddar, sliced, yellow mustard (we used French’s), to serve, 2 tbsp crispy onions",
+    img: "https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2017/07/hot_dogs_steak.jpg?itok=wDwm7Tsf"
   }
 ];
 
