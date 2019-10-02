3b97e88a37b17a9932bc5fe1d3f5acbc536b8941
pusyantekbppt <ahmadnurmus@gmail.com>
2019-10-03 00:34:11 +0700

    add id 15 with flower

--- src/containers/Recipe.tsx
+++ src/containers/Recipe.tsx
@@ -104,6 +104,12 @@ const Recipe = [
     ingredients: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
     img: "https://ih0.redbubble.net/image.694542997.9763/mp,840x830,matte,f8f8f8,t-pad,750x1000,f8f8f8.u2.jpg"
   },
+  {
+    id: 15,
+    title: "Flower",
+    ingredients: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+    img: "https://s7img.ftdi.com/is/image/ProvideCommerce/PF_19_R205_LAY_SHP_V2?$proflowers-tile-wide-mv-new$"
+  },
 ];
 
 export default Recipe;
