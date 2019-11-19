6ead736faca3b5e0657f6243295d7ce41ef2149d
ivan <vansmuhammad@gmail.com>
2019-11-19 16:55:10 +0700

    update: containers

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 
 import { NavWrapper, NavBar } from "styled/nav-styles";
 
-import Recipe from "./Recipe";
-
 import Menu from "components/Menu";
 import Add from "components/Add";
 
@@ -31,23 +29,58 @@ function App() {
     initialState
   );
 
+  const [data_recipe, setDataRecipe] = useState<DataDefinition[]>([]);
+
   const [visibleAdd, setVisibleAdd] = useState<boolean>(false);
   const [visibleUpdate, setVisibleUpdate] = useState<boolean>(false);
 
   const [isDisabled, setDisabled] = useState<boolean>(false);
   const [isRead, setRead] = useState<boolean>(true);
 
-  const [data, setData] = useState<DataDefinition[]>(Recipe);
+  const [isLoadingRecipe, setLoadingRecipe] = useState<boolean>(false);
+  const [isLoadingRecipeAdd, setLoadingRecipeAdd] = useState<boolean>(false);
+
+  const DataJSON = JSON.stringify({
+    title,
+    ingredients,
+    img
+  });
 
   useEffect(() => {
-    if (title === "") {
+    if (!title) {
       return setDisabled(true);
     }
-    if (ingredients === "") {
+    if (!ingredients) {
+      return setDisabled(true);
+    }
+    if (!img) {
       return setDisabled(true);
     }
     return setDisabled(false);
-  }, [title, ingredients]);
+  }, [title, ingredients, img]);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      fetchData();
+    }, 100);
+    return () => clearTimeout(timeOut);
+  }, []);
+
+  const fetchData = async () => {
+    setLoadingRecipe(true);
+    try {
+      const fetching = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}/api/foods/recipe`
+      );
+      const json = await fetching.json();
+      const result = await json.data;
+      setDataRecipe(result);
+      setLoadingRecipe(false);
+      return result;
+    } catch (error) {
+      setLoadingRecipe(false);
+    }
+  };
 
   const handleChangeState = (e: any) => {
     const { id, value } = e.target;
@@ -66,35 +99,48 @@ function App() {
     setRead(!isRead);
   };
 
-  const createRecipe = () => {
-    let newData = {
-      id: Date.now(),
-      title,
-      ingredients,
-      img
-    };
-
-    setData([...data, newData]);
-    setState({ ...initialState });
-    setVisibleAdd(!visibleAdd);
+  const createRecipe = async () => {
+    setLoadingRecipeAdd(true);
+    try {
+      const posting = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}/api/foods/recipe`,
+        {
+          method: "post",
+          body: DataJSON,
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      const json = await posting.json();
+      const result = await json.data;
+      setLoadingRecipeAdd(false);
+      setDataRecipe(result);
+      setState({ ...initialState });
+      setVisibleAdd(!visibleAdd);
+      return result;
+    } catch (error) {
+      setLoadingRecipeAdd(false);
+    }
   };
 
-  const deleteRecipe = (id: number) => {
-    setVisibleUpdate(!visibleUpdate);
-    setData(data.filter(value => value.id !== id));
-  };
+  // const deleteRecipe = (id: number) => {
+  //   setVisibleUpdate(!visibleUpdate);
+  //   setDataRecipe(data.filter(value => value.id !== id));
+  // };
 
   return (
     <div style={{ height: "100vh", margin: "1em" }}>
       <NavWrapper>
         <NavBar>
-          <h1>Box Recipe</h1>
+          <h1>Food Recipe</h1>
           <Add
             title={title}
             ingredients={ingredients}
             img={img}
             visible={visibleAdd}
             isDisabled={isDisabled}
+            isLoadingRecipe={isLoadingRecipeAdd}
             onChangeVisible={handleVisibleAdd}
             onChangeState={handleChangeState}
             onCreate={createRecipe}
@@ -102,12 +148,9 @@ function App() {
         </NavBar>
       </NavWrapper>
       <Menu
-        data={data}
-        isRead={isRead}
-        visible={visibleUpdate}
-        onDelete={deleteRecipe}
+        data_recipe={data_recipe}
+        isLoadingRecipe={isLoadingRecipe}
         onHandleVisible={handleVisibleUpdate}
-        onHandleRead={handleReadOnly}
       />
     </div>
   );
