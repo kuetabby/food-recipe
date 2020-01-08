3eb653b191940b1874a8cbba3a5f6772cb4eafa3
Ivan <vansmuhammad@gmail.com>
2020-01-08 08:50:16 +0700

    update: function parameter

--- public/worker.js
+++ public/worker.js
@@ -1,8 +1,8 @@
 importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");
 // importScripts("../../../dist/umd/comlink.js");
 
-const fetchFunction = async env => {
-  const fetching = await fetch(`${env}/api/foods/recipe`);
+const fetchFunction = async url => {
+  const fetching = await fetch(url);
   const json = await fetching.json();
   const result = await json.data;
   return result;
